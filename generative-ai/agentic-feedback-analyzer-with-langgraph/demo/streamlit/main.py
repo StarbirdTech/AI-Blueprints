286c5cb16a2fc964691fd1136213c8a0c81af182
Ata Turhan <ataturhan21@gmail.com>
2025-07-30 23:33:34 -0500

    Update main.py

--- generative-ai/agentic-feedback-analyzer-with-langgraph/demo/streamlit/main.py
+++ generative-ai/agentic-feedback-analyzer-with-langgraph/demo/streamlit/main.py
@@ -56,7 +56,7 @@ st.sidebar.markdown("""
 """)
 
 endpoint_url = st.sidebar.text_input("MLflow Model Endpoint URL", key="endpoint")
-endpoint_url = 'no need'
+endpoint_url = 'https://localhost:5000/invocations'
 
 if endpoint_url and not endpoint_url.strip().lower().startswith("https://"):
     st.sidebar.error("Endpoint must start with https://")
